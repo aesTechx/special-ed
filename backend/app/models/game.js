fed4be96cfcc440b099a0ec396035153d9789752
eshraq1991 <bakri.eshraq@gmail.com>
2016-08-04 16:02:13 +0300

    Rename and move db models

---
+++ backend/app/models/game.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes){
+	var Game = sequelize.define('Game', {
+		newtiming:DataTypes.INTEGER,
+		besttime:DataTypes.INTEGER,
+		oldtime:DataTypes.INTEGER,
+		repetition:DataTypes.INTEGER,
+		right:DataTypes.INTEGER
+	}, {
+		classMethods: {
+			associate: function(models){
+				Game.hasMany(models.Student);
+			}
+		}
+	});
+	return Game;
+};
