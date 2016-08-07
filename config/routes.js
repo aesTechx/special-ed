8d90746834010d9d35a880bb33c88fd7a72a1e29
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-07 19:24:52 +0400

    Try routing on server

--- config/routes.js
+++ config/routes.js
@@ -3,5 +3,7 @@ var controllers = require('../app/controllers');
 //TODO require event handlers (i.e. database controllers, api controllers, etc)
 
 module.exports = function (app, express) {
-
+	app.get('/api/x', function (req, res, next) {
+		res.json('Hellooooo')
+	})
 };
