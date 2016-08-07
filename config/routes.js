a2a750e3fb7c9e14b1a6b655e6e3e6cc7e34d317
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-07 11:52:38 +0400

    Server to serve client

--- config/routes.js
+++ config/routes.js
@@ -1,19 +1,7 @@
 var helpers = require('./helpers.js'); // our custom middleware
-var controllers = require('../app/controllers/index.js');
+var controllers = require('../app/controllers');
+//TODO require event handlers (i.e. database controllers, api controllers, etc)
+
 module.exports = function (app, express) {
-/////////////////
-<<<<<<< 76ebdbb643a93895dcf2b0af9647ab21d6094292
-=======
-	//app.post('/api/users/signinUser',controllers.Student.addstudent)
-	app.post('/api/forms/submitForm', controllers.Record.addRecord);
->>>>>>> Assessment done
-	app.get('/api/centers', controllers.Center.getAllCenter);
-	app.get('/api/teachers', controllers.Teacher.getAllTeacher);
-	app.get('/api/students', controllers.Student.getAllStudent);
-	app.post('/api/center/addCenter', controllers.Center.addCenter);
-	app.post('/api/teacher/addTeacher', controllers.Teacher.addTeacher);
-	app.post('api/student/addstudent', controllers.Student.addStudent);
-	app.get('/api/games', controllers.Game.getAllGame);
-	// app.use(helpers.errorLogger);
-	// app.use(helpers.errorHandler);
+
 };
