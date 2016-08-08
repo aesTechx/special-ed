119bee49896ff9b453ed5b207879993edfb3c45c
eshraq1991 <bakri.eshraq@gmail.com>
2016-08-08 10:18:21 +0300

    testing controllers

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,5 @@
 var helpers = require('./helpers.js'); // our custom middleware
 var controllers = require('../app/controllers');
-//TODO require event handlers (i.e. database controllers, api controllers, etc)
 
 module.exports = function (app, express) {
 /////////////////
