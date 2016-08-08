46d5ddc15441cabc188a1a63a1f80ff4bac274f1
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-08 19:09:51 +0400

    Add angular ui frontend

---
+++ frontend/helpers/uploadToImgur.js
@@ -0,0 +1,23 @@
+var uploadToIMGUR = function(clientId, imgData, callback) {
+
+	$.ajax({
+		url: 'https://api.imgur.com/3/image',
+		headers: {
+			'Authorization': 'Client-ID' + clientId,
+			'Accept': 'application/json'
+		},
+		type: 'POST',
+		data: {
+			'image': imgData,
+			'type': 'base64'
+		},
+		success: function success(res) {
+
+			if (callback) {
+				callback(res.data);
+			}
+		}
+	});
+};
+
+window.uploadToIMGUR = uploadToIMGUR;
