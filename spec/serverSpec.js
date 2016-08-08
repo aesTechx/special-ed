5ca3a039ff1921967a9d389d9e6f12a8c0086dde
eshraq1991 <bakri.eshraq@gmail.com>
2016-08-08 12:39:58 +0300

    Modify controllers for tests

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -5,15 +5,9 @@ var expect = require('chai').expect;
 var server = require(path.join(__dirname, '..', './server.js'));
 
 describe('server()', function () {
-	'use strict';
+  'use strict';
 	it('exists', function () {
 		expect(server).to.be.a('function');
-	});
-	it('does something', function () {
-		expect(true).to.equal(false);
-	});
-	it('does something else', function () {
-		expect(true).to.equal(false);
 	});
 });
 
