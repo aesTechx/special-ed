ad3603b334c0b0562a184a9c7ef365f07a684db6
eshraq1991 <bakri.eshraq@gmail.com>
2016-08-08 12:39:58 +0300

    Modify controllers for tests

---
+++ backend/spec/serverSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+var path = require('path');
+var expect = require('chai').expect;
+
+var server = require(path.join(__dirname, '..', './server.js'));
+
+describe('server()', function () {
+  'use strict';
+	it('exists', function () {
+		expect(server).to.be.a('function');
+	});
+});
+
