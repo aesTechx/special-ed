f616785059147653368e4007ed19dfb96f75b71f
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-08 19:10:09 +0400

    Recreate repo

--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,18 @@
 var helpers = require('./helpers.js'); // our custom middleware
-var models=require('../app/models/center.js');
+var models = require('../app/models/center.js');
 //console.log(models);
 var controllers = require('../app/controllers/index.js');
 
 module.exports = function (app, express) {
 
 	app.get('/api/centers', controllers.Center.getAllCenter);
-	app.get('/api/teachers',controllers.Teacher.getAllTeacher);
-	app.get('/api/students',controllers.Student.getAllStudent);
-	app.post('/api/center/addCenter',controllers.Center.addCenter);
-	app.post('/api/teacher/addTeacher',controllers.Teacher.addTeacher);
-	app.post('api/student/addstudent',controllers.Student.addStudent);
-	app.get('/api/games',controllers.Game.getAllGame)
+	app.get('/api/teachers', controllers.Teacher.getAllTeacher);
+	app.get('/api/students', controllers.Student.getAllStudent);
+	app.post('/api/center/addCenter', controllers.Center.addCenter);
+	app.post('/api/teacher/addTeacher', controllers.Teacher.addTeacher);
+	app.post('api/student/addstudent', controllers.Student.addStudent);
+	app.get('/api/games', controllers.Game.getAllGame);
+///////////
 	// app.use(helpers.errorLogger);
 	// app.use(helpers.errorHandler);
-}
+};
