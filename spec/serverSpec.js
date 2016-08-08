47a3d044345a30ff6aa6657ddfad423d198e9bb6
eshraq1991 <bakri.eshraq@gmail.com>
2016-08-08 12:39:58 +0300

    Modify controllers for tests

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -6,20 +6,8 @@ var server = require(path.join(__dirname, '..', './server.js'));
 
 describe('server()', function () {
   'use strict';
-
-  // it('exists', function () {
-  //   expect(server).to.be.a('function');
-
-  // });
-
-  // it('does something', function () {
-  //   expect(true).to.equal(false);
-  // });
-
-  // it('does something else', function () {
-  //   expect(true).to.equal(false);
-  // });
-
-  // Add more assertions here
+	it('exists', function () {
+		expect(server).to.be.a('function');
+	});
 });
 
