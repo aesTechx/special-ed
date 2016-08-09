5137dd1645ffbff67726f84e5df582181beb0942
AwsAhmed <aws.y.ahmed@gmail.com>
2016-08-09 15:13:07 +0300

    add the form page and controller

--- frontend/app/user/user.js
+++ frontend/app/user/user.js
@@ -1,26 +1,8 @@
 angular.module('SED.Users', [])
 
-.controller('UserController', function ($scope, Orders, Auth) {
-  // Your code here
-  ///this function must call on submit click
-  // $scope.order = {};
-  // $scope.order.totalPrice = 0;
-  // var prices = {gasCylinder: 8, water: 1, diesel: 5};
-  // $scope.order.serviceType;
-  // $scope.order.username;
-  // $scope.getVal=function(event){
-  //     $scope.order.serviceType = event.currentTarget.value;
-  // }
-  //   $scope.order.totalPrice = parseInt($scope.order.quantity) * prices[$scope.order.serviceType];
-  // $scope.addOrder=function(){
-  // 	Orders.addOneOrder($scope.order).then(function(){
-  //     //console.log($scope.order)
-  // 	})
-  // 	.catch(function(err){
-  // 		console.log(err);
-  // 	})
-  // }
-  // $scope.signout=function(){
-  //   Auth.signoutUser();
-  // }
+.controller('UserController', function ($scope,Auth) {
+
+  $scope.signout=function(){
+    Auth.signoutUser();
+  }
 });
