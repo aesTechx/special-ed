deac05f5b98dff1dc1ad922d7f38fa10f9c73fe8
AwsAhmed <aws.y.ahmed@gmail.com>
2016-08-10 17:27:19 +0300

    Assessment done

--- spec/frontend/Spec.js
+++ spec/frontend/Spec.js
@@ -11,11 +11,11 @@
 // 		})
 // 	});
 // })
-describe('Filters', function (){
+describe('Filters', function () {
 	beforeEach(module('SED'));
 	describe('attach tokens', function () {
 		var attach;
-		beforeEach(inject(function(_AttachTokens_){
+		beforeEach(inject(function(_AttachTokens_) {
 			attach = _AttachTokens_;
 		}));
 		it ('should attach a string', function () {
@@ -23,4 +23,5 @@ describe('Filters', function (){
 			// expect(typeof attach.request).toBe('function');
 		});
 	});
+
 });
