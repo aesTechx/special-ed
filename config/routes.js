b59dac77d4f7c314ece67afb0da9c37162d97a74
Eshraq1991 <bakri.eshraq@gmail.com>
2016-08-10 17:23:18 +0400

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,9 @@
 var helpers = require('./helpers.js'); // our custom middleware
 var models = require('../app/models/center.js');
-//console.log(models);
 var controllers = require('../app/controllers/index.js');
-//TODO require event handlers (i.e. database controllers, api controllers, etc)
-// var app = express();
-module.exports = function (app, express) {
-<<<<<<< d17aab4e19a1835304b38169c4b1cca8bbf76b86
 
-=======
+module.exports = function (app, express) {
 /////////////////
->>>>>>> Recreate repo
 	app.get('/api/centers', controllers.Center.getAllCenter);
 	app.get('/api/teachers', controllers.Teacher.getAllTeacher);
 	app.get('/api/students', controllers.Student.getAllStudent);
@@ -17,7 +11,6 @@ module.exports = function (app, express) {
 	app.post('/api/teacher/addTeacher', controllers.Teacher.addTeacher);
 	app.post('api/student/addstudent', controllers.Student.addStudent);
 	app.get('/api/games', controllers.Game.getAllGame);
-///////////
 	// app.use(helpers.errorLogger);
 	// app.use(helpers.errorHandler);
 };
