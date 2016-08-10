f75200f117201fcdf93bf5d92bc9b4589009523a
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-11 00:26:06 +0400

    Edit login code to match new changes to html

--- frontend/scripts/controllers/login.js
+++ frontend/scripts/controllers/login.js
@@ -1,20 +1,51 @@
 'use strict';
-
-/**
- * @ngdoc function
- * @name yapp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of yapp
- */
 angular.module('SED')
-  .controller('LoginCtrl', function($scope, $location) {
-
+  .controller('LoginCtrl', function($scope, $location, $window, Auth) {
+  	$scope.user = {};
+  	$scope.images = {
+  		'Student': "images/flat-avatar.png", 
+  		'Teacher': "images/teacher-avatar.jpg", 
+  		'Center': "images/school-minions.jpg"
+  	}
+  	$scope.changeSelect = function (){
+      $scope.user = {};
+  	}
     $scope.submit = function() {
+    	var option = $scope.option
+    	console.log(option, $scope.user)
+    	if (option === 'Teacher') {
+    		Auth.signinTeacher($scope.user)
+			.then(function (token) {
+				console.log(token)
+				$window.localStorage.setItem('com.SEDteacher', token);
+				// $location.path('/teacher');
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+    	} else if (option === 'Student') {
+    		Auth.signinUser($scope.user)
+			.then(function (token) {
+				console.log(token)
 
-      $location.path('/dashboard');
+				$window.localStorage.setItem('com.SEDuser', token);
+				// $location.path('/user');
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+    	} else if (option === 'Center') {
+			Auth.signinCenter($scope.user)
+			.then(function(token){
+				console.log(token)
 
-      return false;
+				$window.localStorage.setItem('com.SEDcenter',token);
+				// $location.path('/center');
+			})
+			.catch(function(error){
+				console.error(error);
+			});
+    	}
+    	$location.path('/dashboard');
     }
-
-  });
+})
