c7a5703d5e04806bd843a68959cb82c4168a63c8
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-11 00:26:58 +0400

    Revamp directories to reflect new changes

--- frontend/app/teacher/teacher.js
+++
@@ -1,8 +0,0 @@
-angular.module('SED.Teachers', [])
-
-.controller('teacherController', function ($scope, Services,Auth) {
-  $scope.data={};
-  $scope.signout=function(){
-    Auth.signoutTeacher();
-  }
-});
