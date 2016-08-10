f990791b622e4537d03fd8431321e886e1fae5ec
Eshraq1991 <bakri.eshraq@gmail.com>
2016-08-10 17:23:18 +0400

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,7 @@
 var helpers = require('./helpers.js'); // our custom middleware
 var models = require('../app/models/center.js');
-//console.log(models);
 var controllers = require('../app/controllers/index.js');
-//TODO require event handlers (i.e. database controllers, api controllers, etc)
-// var app = express();
+
 module.exports = function (app, express) {
 	app.get('/api/centers', controllers.Center.getAllCenter);
 	app.get('/api/teachers', controllers.Teacher.getAllTeacher);
