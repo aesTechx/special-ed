ecd50eaca621aaee4e8bace110303b39649e3f16
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-10 16:45:24 +0400

    Create landing page theme

--- spec/server/controllerSpec.js
+++ spec/server/controllerSpec.js
@@ -3,30 +3,30 @@ var app = require(path.join(__dirname, '..', '..', './server.js'));
 var controller = require(path.join(__dirname,'..','..','./app/controllers/index.js'));
 var should = require('chai').should();
 var chai = require('chai');
-var chaihttp=require('chai-http');
+var chaihttp = require('chai-http');
 chai.use(chaihttp);
 var models = require(path.join(__dirname, '..', '..', './app/models'));
 
-	describe('centerGetPost()', function () {
-		'use strict';
+describe('centerGetPost()', function () {
+	'use strict';
 		
-		it('post a new center to DB',function(){
-		 chai.request(app)
-				.post('http://127.0.0.1:8000/api/center/addCenter')
-				.send({centername:'eshraq',password:'eshraq',username:'eshraq'})
-				.end(function(err,res){
-					res.should.have.status(201);
-					res.should.be.json;
-					res.body.should.have.property('SUCCESS');
-					res.body.SUCCESS.should.be.a('object');
+	it('post a new center to DB', function(){
+	chai.request(app)
+		.post('http://127.0.0.1:8000/api/center/addCenter')
+		.send({centername:'eshraq',password:'eshraq',username:'eshraq'})
+		.end(function(err,res){
+			res.should.have.status(201);
+			res.should.be.json;
+			res.body.should.have.property('SUCCESS');
+			res.body.SUCCESS.should.be.a('object');
 					// res.body.SUCCESS.should.have.property('username');
 					// res.body.SUCCESS.should.have.property('centername');
 					// res.body.SUCCESS.should.have.property('password');
 					// res.body.SUCCESS.should.have.property('id');
 					// res.body.SUCCESS.username.should.equal('eshraq');
 					// res.body.SUCCESS.centername.should.equal('eshraq');
-					res.body.should.be.a('object');
-					done();
+			res.body.should.be.a('object');
+			done();
 				});
 		});
 		it('request all centers from the DB',function(done){
@@ -48,6 +48,22 @@ var models = require(path.join(__dirname, '..', '..', './app/models'));
 		});
 	});
 
+describe('index()', function () {
+	'use strict';
+	it('exists', function () {
+		expect(index).to.be.a('function');
+	});
+	it('does something', function () {
+		expect(true).to.equal(false);
+	});
+	it('does something else', function () {
+		expect(true).to.equal(false);
+	});
+	it('post a student to student schema', function() {
+		request;
+	});
+  // Add more assertions here
+});
 	describe('studentGetPost()', function () {
 		'use strict';
 		it('post a new student to DB',function(){
