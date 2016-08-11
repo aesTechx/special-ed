309d3e6ceb0ddfcabc592b2028192cda9cc3c893
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-11 06:59:51 +0400

    Resolve after merge failures

--- frontend/scripts/controllers/center.js
+++
@@ -1,8 +0,0 @@
-angular.module('SED.Centers', [])
-
-.controller('centerController', function ($scope, Services,Auth) {
-  $scope.data={};
-  $scope.signout=function(){
-    Auth.signoutCenter();
-  }
-});
