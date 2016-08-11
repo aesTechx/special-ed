b2a20ffe66e8363cab1bd0e4348f25a63de3e72b
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-11 05:36:30 +0400

    Fix styling

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,51 +2,70 @@ var db = require('../models');
 var jwt = require('jwt-simple');
 
 module.exports = {
-  _getAll: function(field){
-    db[field].findAll()
-      .then(function(results){
-      return results;
-    })
-  },
-	Center: {
-		getAllCenter: function (req, res) {
-			db.Center.findAll()
-			.then(function(centers) {
-				res.json(centers);
-			});
-		},
-	    addCenter: function (req, res) {
+
+  Center: {
+    getAllCenter: function (req, res) {
+      db.Center.findAll()
+      .then(function(centers) {
+        res.status(200);
+        res.send(centers);
+      });
+    },
+    addCenter: function (req, res) {
 			// findOrCreate returns multiple resutls in an array
 			// use spread to assign the array to function arguments
-			db.Center.create({
-				centername: req.body.centername,
-				username: req.body.fullname,
-				password: req.body.password
-			})
-			.then(function(message) {
-				res.sendStatus(201);
-			});
-		}
-	},
-	Student: {
-		getAllStudent: function(req, res) {
-			db.Student.findAll()
-			.then(function(students) {
-				res.json(students);
-			});
-		},
-		addStudent: function(req, res) {
-			db.Student.create({
-				username: req.body.username,
-				fullname: req.body.fullname,
-				skillsResult: req.body.skillsResult,
-				birthDate: req.body.birthDate
-			})
-			.then(function() {
-				res.sendStatus(201);
-			});
-		}
-	},
+      db.Center.create({
+        centername: req.body.centername,
+        username: req.body.fullname,
+        password: req.body.password
+      }).then(function(center) {
+        var token = jwt.encode(center, 'secret');
+        res.json({token: token});
+      });
+    },
+    signinCenter: function (req, res) {
+      var username = req.body.username;
+      var password = req.body.password;
+      db.Center.findOne({ where: { username: username, password: password }})
+      .then(function(user) {
+        if (user !== null) {
+          var token = jwt.encode(user, 'secret');
+          res.json({ token: token });
+        }
+      });
+    }
+  },
+  Student: {
+    getAllStudent: function (req, res) {
+      db.Student.findAll()
+      .then(function(students) {
+        res.json(students);
+      });
+    },
+    addStudent: function (req, res) {
+      db.Student.create ({
+        username: req.body.username,
+        fullname: req.body.fullname,
+        skillsResult: req.body.skillsResult,
+        birthdate: req.body.birthdate
+      })
+      .then(function(student) {
+        var token = jwt.encode(student, 'secret');
+        res.json({token: token});
+      });
+    },
+    signinStudent: function(req, res) {
+      var username = req.body.username;
+      var password = req.body.password;
+      db.Center.findOne({ where: {username: username, password: password}})
+			.then( function (user) {
+        if (user !== null) {
+          var token = jwt.encode(user, 'secret');
+          res.json({token: token});
+        }
+      });
+    }
+  },
 	Teacher: {
 		getAllTeacher: function(req, res) {
 			db.Teacher.findAll()
@@ -56,15 +75,33 @@ module.exports = {
 		},
 		addTeacher: function(req, res) {
 			db.Teacher.create({
-				username: req.body.username,
+				teachername: req.body.teachername,
 				fullname: req.body.fullname,
 				category: req.body.category,
 				password: req.body.password
 			})
-			.then(function() {
-				res.sendStatus(201);
-			});
-		}
+			.then(function(teacher) {
+				var token = jwt.encode(teacher, 'secret');
+        		res.json({token: token});
+			})
+			.catch(function(error){
+				console.log(error);
+			})
+		},
+	  	signinTeacher:function(req, res) {
+	  		var teachername = req.body.username;
+	    	var password = req.body.password;
+	    	db.Center.findOne({ where: {teachername: teachername, password: password }})
+	    	.then(function(user) {
+	    		if(user!== null){
+	    			var token = jwt.encode(user, 'secret');
+	          res.json({token: token});
+	    		}
+	      })
+	      .catch(function(err){
+	        res.json(err)
+	      })
+	  }
 	},
 	Game: {
 		getAllGame: function(req, res) {
@@ -83,10 +120,7 @@ module.exports = {
 				res.json(records);
 			});
 		},
-		
-
 		addRecord: function(req, res) {
-			console.log(req.body.SOCIAL)
 			db.Record.create({
 				SOCIAL: req.body.SOCIAL,
 				PERSEVERATION: req.body.PERSEVERATION,
