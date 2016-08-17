fb60204abda4175b2b7a6203347c841ed425e86f
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-17 18:15:16 +0400

    Write test files

--- spec/frontend/controllers/signupSpec.js
+++ spec/frontend/controllers/signupSpec.js
@@ -5,20 +5,47 @@ describe('SignUp Controller', function() {
   });
 
   var $controller;
-
-  beforeEach(inject(function(_$controller_){
+  var Auth;
+  var controller;
+  var $q;
+  beforeEach(inject(function(_$controller_, _Auth_, _$q_){
     // The injector unwraps the underscores (_) from around the parameter names when matching
+     // $q = _$q_
     $controller = _$controller_;
+    // Auth = _Auth_;
   }));
 
   describe('change selection', function() {
     it('sets selected student to true', function() {
       var $scope = {};
-      var controller = $controller('SignupCtrl', { $scope: $scope });
-      console.log(controller)
+      controller = $controller('SignupCtrl', { $scope: $scope });
       $scope.option = 'Student';
       $scope.changeSelect();
       expect($scope.studentSelected).toEqual(true);
     });
   });
+  describe('submit to server', function () {
+    it ('if option is Teacher it should send to signupTeacher', function () {
+      var $scope = {};
+      var hasBeenCalled = false;
+      var promise = function(user) {return new Promise(function (resolve, reject) {
+          hasBeenCalled = true;
+          return resolve(user);
+      })};
+      var Auth = {signupTeacher: promise};
+      // spyOn(Auth, 'signupTeacher').and.callFake(function(user){
+      //   return resolve(user)
+      // })
+      // jasmine.spyOn(Auth, 'signupTeacher').andCallFake(function(){
+      //   var deferred = $q.defer();
+      //   return deferred.promise;
+      // });
+      controller = $controller('SignupCtrl', { $scope: $scope, Auth:Auth});
+      $scope.option = 'Teacher';
+      $scope.user = {username:'x'};
+      $scope.submit();
+      console.log(Auth)
+      expect(hasBeenCalled).toEqual(true);
+    })
+  })
 });
