afc8a5cf2444543dd0a222ad9dc5e96116b2bfaf
AwsAhmed <aws.y.ahmed@gmail.com>
2016-08-21 23:46:08 +0300

    minor fixes remove the script from the landing page to the controller

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -61,7 +61,6 @@ angular.module('SED', [
   var attach = {
     request: function (object) {
       var jwt = $window.localStorage.getItem('com.SEDuser') || $window.localStorage.getItem('com.SEDteacher') || $window.localStorage.getItem('com.SEDcenter');
-      console.log(jwt);
       if (jwt) {
         object.headers['x-access-token'] = jwt;
       }
