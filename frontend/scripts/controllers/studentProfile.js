ba642425b5b083cae92e54596016919e7e1bde58
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-23 14:37:40 +0400

    Remove console logs

--- frontend/scripts/controllers/studentProfile.js
+++ frontend/scripts/controllers/studentProfile.js
@@ -1,76 +1,70 @@
 angular.module('SED.studentProfile', [])
 .controller('studentCtrl', function($scope, $state, Auth, Centers, Students) {
- $scope.edit=false;
- $scope.data={};
- $scope.user={};
- $scope.teachers=false;
+ $scope.edit = false;
+ $scope.data = {};
+ $scope.user = {};
+ $scope.teachers = false;
  $scope.record = { social: 0, preservation: 0, communicationAndDevelopment: 0, sensoryDisturbance: 0, attentionAndSafety: 0 };
   Centers.getAllCenters()
   .then(function(centers){
-    $scope.data.centers=centers;
+    $scope.data.centers = centers;
   });
   Students.getCurrentStudent()
   .then(function(resp){
-    $scope.data.student=resp;
+    $scope.data.student = resp;
     if ($scope.data.student.birthdate) {
       $scope.data.student.birthdate.toString();
       $scope.data.student.birthdate = $scope.data.student.birthdate.substr(0,10);
     } 
   });
-  $scope.center=function(){
+  $scope.center = function() {
     console.log("hello")
   }
-  $scope.viewTeachers=function(){
-    $scope.teachers=true;
-    console.log("viewTeachers")
+  $scope.viewTeachers = function() {
+    $scope.teachers = true;
   	Students.viewTeachers()
   	.then(function(resp){
-  		$scope.data.teachers=resp;
-  		console.log(resp);
+  		$scope.data.teachers = resp;
   	});
   }
-   $scope.viewGames=function(){
+   $scope.viewGames = function(){
     Students.viewGames()
   	.then(function(resp){
-  		$scope.data.teachers=resp;
-  		console.log(resp);
+  		$scope.data.teachers = resp;
   	});
   }
-  $scope.viewRecords=function(){
+  $scope.viewRecords = function(){
   	Students.viewRecords()
-  	 .then(function(resp){
-  	 	$scope.data.records=resp;
-  	 	console.log(resp);
-  	 });
+    .then(function(resp){
+      $scope.data.records=resp;
+    });
   }
   $scope.editflag=function(){
     $scope.edit=true;
   }
   $scope.editProfile=function(){
-    console.log($scope.user)
     Students.editProfile($scope.user)
       .then(function(resp){
-        console.log(resp)
         $scope.data.student=resp;
-         $scope.data.student.birthdate.toString();
-         $scope.data.student.birthdate=$scope.data.student.birthdate.substr(0,10);
+        $scope.data.student.birthdate.toString();
+        $scope.data.student.birthdate=$scope.data.student.birthdate.substr(0,10);
       });
   $scope.edit=false;
   }
   Students.getRecords()
-   .then(function(resp){
+  .then(function(resp){
     for(var i=0;i<resp.length;i++){
       if(resp[i].social !== undefined && resp[i].preservation !== undefined && resp[i].communicationAndDevelopment!==undefined && resp[i].sensoryDisturbance!== undefined && resp[i].attentionAndSafety !== undefined){
-          $scope.record.social = resp[i].social;
-          $scope.record.preservation = resp[i].preservation;
-          $scope.record.communicationAndDevelopment = resp[i].communicationAndDevelopment;
-          $scope.record.sensoryDisturbance = resp[i].sensoryDisturbance;
-          $scope.record.attentionAndSafety = resp[i].attentionAndSafety;
+        $scope.record.social = resp[i].social;
+        $scope.record.preservation = resp[i].preservation;
+        $scope.record.communicationAndDevelopment = resp[i].communicationAndDevelopment;
+        $scope.record.sensoryDisturbance = resp[i].sensoryDisturbance;
+        $scope.record.attentionAndSafety = resp[i].attentionAndSafety;
+      }
     }
-  }
-      $scope.initialize();
-   })
-    $scope.initialize = function () {
+    $scope.initialize();
+  })
+  $scope.initialize = function () {
     var loadLiquidFillGauge = window.loadLiquidFillGauge;
     var overall = ((($scope.record.social + 
                   $scope.record.preservation + 
