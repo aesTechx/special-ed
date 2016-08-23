5ca40d7af5b541a581ac567cb05fedab09e6e843
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-08-23 11:42:41 +0400

    Remove picture

--- frontend/scripts/controllers/login.js
+++ frontend/scripts/controllers/login.js
@@ -3,13 +3,6 @@ angular.module('SED.Login', [])
 .controller('LoginCtrl', function LoginCtrl($scope, $window, Auth, $location) {
   $scope.user = {};
   $scope.option = 'Select Type of User';
-  $scope.images = {
-    'Student': 'images/flat-avatar.png', 
-    'Teacher': 'images/teacher-avatar.jpg', 
-    'Center': 'images/school-minions.jpg'
-  };
-  $scope.changeSelect = function () {
-  };
   $scope.submit = function() {
     var option = $scope.option;
     Auth['signin' + $scope.option]($scope.user)
